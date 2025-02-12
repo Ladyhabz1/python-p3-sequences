ec61f3d4a4c050419ba85c8d781d975051d4c696
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2025-02-12 17:17:44 +0300

    Complete labs

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1] if length > 1 else [0] if length == 1 else []
+    
+    while len(fibonacci) < length:
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    
+    print(fibonacci)  # Ensure it prints as a list
+
+
+print_fibonacci(10) # Expected Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
